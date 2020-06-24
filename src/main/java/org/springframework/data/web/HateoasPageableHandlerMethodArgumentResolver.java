a3e6529d12b8c5f919b1bd67ae853d56caa4d38a
Réda Housni Alaoui <reda-alaoui@hey.com>
2020-06-24 15:41:21 +0200

    #2168 No TemplateVariable generated for null Pageable

--- src/main/java/org/springframework/data/web/HateoasPageableHandlerMethodArgumentResolver.java
+++ src/main/java/org/springframework/data/web/HateoasPageableHandlerMethodArgumentResolver.java
@@ -40,6 +40,7 @@ import org.springframework.web.util.UriComponentsBuilder;
  * @since 1.6
  * @author Oliver Gierke
  * @author Nick Williams
+ * @author Réda Housni Alaoui
  */
 @SuppressWarnings("null")
 public class HateoasPageableHandlerMethodArgumentResolver extends PageableHandlerMethodArgumentResolver
@@ -121,6 +122,11 @@ public class HateoasPageableHandlerMethodArgumentResolver extends PageableHandle
 				pageable.getPageSize() <= getMaxPageSize() ? pageable.getPageSize() : getMaxPageSize());
 
 		this.sortResolver.enhance(builder, parameter, pageable.getSort());
+	}
+
+	@Override
+	public TemplateVariables enhance(TemplateVariables templateVariables, UriComponents uriComponents, MethodParameter parameter) {
+		return templateVariables.concat(getPaginationTemplateVariables(parameter, uriComponents));
 	}
 
 	private static HateoasSortHandlerMethodArgumentResolver getDefaultedSortResolver(
