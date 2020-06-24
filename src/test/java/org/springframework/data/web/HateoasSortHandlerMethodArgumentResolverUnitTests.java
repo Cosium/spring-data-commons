3a48df90b69ae71079d4caa6a4c200e3dd04dd27
Réda Housni Alaoui <reda-alaoui@hey.com>
2020-06-24 15:41:21 +0200

    #2168 No TemplateVariable generated for null Pageable

--- src/test/java/org/springframework/data/web/HateoasSortHandlerMethodArgumentResolverUnitTests.java
+++ src/test/java/org/springframework/data/web/HateoasSortHandlerMethodArgumentResolverUnitTests.java
@@ -23,12 +23,15 @@ import java.net.URI;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Unit tests for {@link HateoasSortHandlerMethodArgumentResolver}
  *
  * @author Oliver Gierke
+ * @author Réda Housni Alaoui
  */
 class HateoasSortHandlerMethodArgumentResolverUnitTests extends SortHandlerMethodArgumentResolverUnitTests {
 
@@ -54,6 +57,15 @@ class HateoasSortHandlerMethodArgumentResolverUnitTests extends SortHandlerMetho
 
 		var resolver = new HateoasSortHandlerMethodArgumentResolver();
 		assertThat(resolver.getSortTemplateVariables(null, uriComponents).toString()).isEqualTo("{?sort}");
+	}
+
+	@Test // DATACMNS-1752
+	void enhanceTemplateVariables(){
+
+		UriComponents uriComponents = UriComponentsBuilder.fromPath("/").build();
+
+		HateoasSortHandlerMethodArgumentResolver resolver = new HateoasSortHandlerMethodArgumentResolver();
+		assertThat(resolver.enhance(TemplateVariables.NONE, uriComponents, null).toString()).isEqualTo("{?sort}");
 	}
 
 	private void assertUriStringFor(Sort sort, String expected) throws Exception {
